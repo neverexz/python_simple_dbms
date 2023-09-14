0bb42b28313fac8e6d44a965179d79eae50cc79f
Кулагин Егор <cool-eg@mail.ru>
2023-09-14 13:56:06 +0300

    fin

--- db.py
+++ db.py
@@ -20,7 +20,7 @@ def authorize():
     DATABASE = input()
     try:
         create_connection()
-        print('Успешно')
+        print('Авторизация прошла успешно')
         return 1
     except pymysql.Error as e:
         print('Ошибка ', e)
@@ -45,20 +45,76 @@ def base_select():
     except pymysql.Error as e:
         print("Ошибка ", e)
 
-"""main"""
+def show_grants():
+    try:
+        with CONNECTION.cursor() as cursor:
+            sql = f"SHOW GRANTS FOR '{USER}'@'localhost';"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for res in result:
+                print(res)
+    except pymysql.Error as e:
+        print("Ошибка ", e)
+
+def create_user():
+    try:
+        with CONNECTION.cursor() as cursor:
+            print(f'Введите имя нового пользователя: ')
+            user = input()
+            print(f'Введите пароль нового пользователя: ')
+            password = input()
+            sql = f"CREATE USER '{user}'@'localhost' IDENTIFIED BY '{password}';"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for res in result:
+                print(res)
+            print('Пользователь успешно создан')
+    except pymysql.Error as e:
+        print("Ошибка ", e)
+
+def give_grants():
+    try:
+        with CONNECTION.cursor() as cursor:
+            print(f'Введите имя пользователя: ')
+            user = input()
+            print(f'Введите комманды для доступа в формате: "cmd1, cmd2, etc."')
+            commands = input()
+            print(f'Введите название таблицы')
+            table = input()
+            sql = f"grant {commands} ON {DATABASE}.{table} TO '{user}'@'localhost';"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for res in result:
+                print(res)
+            print('Права выданы')
+    except pymysql.Error as e:
+        print("Ошибка ", e)
+
+
 print('Добро пожаловать в модуль управления SQL')
 authorized = 0
 while True:
     print('Нажмите 1 для авторизации пользователя')
-    print('Нажмите 2 для выполнения запроса')
+    if authorized:   
+        print('Нажмите 2 для выполнения запроса')
+        print('Нажмите 3 для просмотра своих прав')
+        print('Нажмите 4 чтобы создать нового пользователя')
+        print('Нажмите 5 чтобы выдать права пользователю')
     print('Нажмите 404 для выхода')
-    statement = int(input())
-    if statement==1:
+    statement = input()
+    print("\033[H\033[J")
+    if statement=='1':
         authorized = authorize()
-    elif statement==2 and authorized:
+    elif statement=='2' and authorized:
         base_select()
-    elif statement==404:
-        print('пока')
+    elif statement=='3' and authorized:
+        show_grants()
+    elif statement=='4' and authorized:
+        create_user()
+    elif statement=='5' and authorized:
+        give_grants()
+    elif statement=='404':
+        print('выход из программы')
         break
     else:
         print('error')
